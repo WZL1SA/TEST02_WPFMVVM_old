c8633b8fb29437e397819941abece72a589fbce2
Witold Majda <witoldmajda@outlook.com>
2018-03-26 14:45:09 +0200

    dodanie klas

---
+++ TEST02_WPFMVVM/viewmodel/CustomerViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST02_WPFMVVM.interfaces;
+using TEST02_WPFMVVM.model;
+using TEST02_WPFMVVM.services;
+
+namespace TEST02_WPFMVVM.viewmodel
+{
+    public class CustomerViewModel : BaseViewModel
+    {
+        private readonly ICustomersService _CustomersService;
+
+        public CustomerViewModel(ICustomersService _CustomersService)
+        {
+            this._CustomersService = _CustomersService;
+
+            _Customer = new CustomerModel("Witek");
+           
+
+            //Customers = new ObservableCollection<CustomerModel>(_CustomersService.Get());  //implementacja klasy informującej listę o konieczności zmiany
+            Load();
+
+        }
+
+        public CustomerViewModel()
+            : this(new MocCustomersServices()) // gdy kożystamy z Moc używamy metod Moc
+                                               //: this(new DbProductService()) // gdy kożystamy z bazy danych kożystamy z metod klasy DbProductService
+        {
+
+        }
+
+
+        private void Load()
+        {
+            Customers = new ObservableCollection<CustomerModel>(_CustomersService.Get());  //implementacja klasy informującej listę o konieczności zmiany
+        }
+
+        private ICollection<CustomerModel> _Customers;
+
+        public ICollection<CustomerModel> Customers
+        {
+            get { return _Customers; }
+            set
+            {
+                _Customers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private CustomerModel _Customer;
+
+        public CustomerModel Customer
+        {
+            get { return _Customer; }
+            set
+            {
+                _Customer = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    }
