93d93bb095737172558aa1e5b8f9d2ded05bcb16
Witold Majda <witoldmajda@outlook.com>
2018-03-30 11:52:15 +0200

    Dodanie funkcjonalności po kliknięciu w wiersz tabeli

--- TEST02_WPFMVVM/Commands/RelayCommand.cs
+++ TEST02_WPFMVVM/Commands/RelayCommand.cs
@@ -44,4 +44,39 @@ namespace TEST02_WPFMVVM.Commands
 
         }
     }
+
+    //public class RelayCommand : ICommand
+    //{
+    //    private Action execute;
+    //    private Func<bool> canExecute;
+
+    //    public event EventHandler CanExecuteChanged
+    //    {
+    //        add { CommandManager.RequerySuggested += value; }
+    //        remove { CommandManager.RequerySuggested -= value; }
+    //    }
+
+    //    public RelayCommand(Action execute, Func<bool> canExecute = null)
+    //    {
+    //        this.execute = execute;
+    //        this.canExecute = canExecute;
+    //    }
+
+    //    public bool CanExecute(object parameter)
+    //    {
+    //        if (canExecute == null)
+    //        {
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return canExecute.Invoke();
+    //        }
+    //    }
+
+    //    public void Execute(object parameter)
+    //    {
+    //        this.execute.Invoke();
+    //    }
+    //}
 }
